8a5925f9397bfe1e45ad9e31ff8a925b5480e6ee
Ion <19718355+Nei-V@users.noreply.github.com>
2018-07-21 21:24:06 +0300

    readme updated

--- src/actions/action1.js
+++ src/actions/action1.js
@@ -4,6 +4,6 @@ export const allText = (text) => dispatch => {
     console.log("text in action1 alltext : ",text);
     dispatch ({
         type: NEWTEXT,
-        fullText:text
+        fullText: text
     })
 }
